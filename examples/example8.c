33bfc9816f3f43de73983bc7ba949a8b88f8ebf1
Quentin18 <quentindeschamps18@orange.fr>
2021-02-27 00:02:43 +0100

    add example8

---
+++ examples/example8.c
@@ -0,0 +1,34 @@
+/**
+ * @file example8.c
+ * @brief Example 8: Advanced operations with polynomial arithmetic in Fm[x]/P
+ */
+#include <stdio.h>
+#include "modpolyp.h"
+
+/**
+ * Advanced operations with polynomial arithmetic in Fm[x]/P.
+ */
+int main()
+{
+    int dp, dq, dr, ds, dP, m;
+    polynomial r, s;
+
+    polynomial p = {1, 1, 0, 0, 1};
+    polynomial q = {1, 0, 1, 1, 1};
+    polynomial P = {1, 0, 1, 0, 0, 1};
+
+    dp = 4, dq = 4, dP = 5, m = 2;
+
+    p_print(p, dp, "P");
+    p_print(q, dq, "Q");
+
+    /* Inverse */
+    dr = mpp_inv(p, dp, r, m, P, dP);
+    p_print(r, dr, "P^-1");
+
+    /* Division */
+    ds = mpp_div(p, dp, q, dq, s, m, P, dP);
+    p_print(s, ds, "P / Q");
+
+    return 0;
+}
