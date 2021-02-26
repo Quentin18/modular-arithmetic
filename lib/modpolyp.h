8d3d0bd901ec98ad3f411686a4c06b6655e06e19
Quentin18 <quentindeschamps18@orange.fr>
2021-02-26 18:03:51 +0100

    update header modpolyp

--- lib/modpolyp.h
+++ lib/modpolyp.h
@@ -3,3 +3,19 @@
  * @brief Header file for polynomial arithmetic in Fm[x]/P
  */
 #pragma once
+
+#include "polynomial.h"
+
+/* Definition */
+
+int mpp_init(polynomial p, char *name);
+void mpp_print(const polynomial p, int dp, char *name);
+int mpp_copy(const polynomial p, int dp, polynomial r);
+
+/* Operators */
+
+int mpp_add(const polynomial p, int dp, const polynomial q, int dq, polynomial r, int m, polynomial P);
+int mpp_sub(const polynomial p, int dp, const polynomial q, int dq, polynomial r, int m, polynomial P);
+int mpp_mul(const polynomial p, int dp, const polynomial q, int dq, polynomial r, int m, polynomial P);
+int mpp_inv(const polynomial p, int dp, const polynomial q, int dq, polynomial r, int m, polynomial P);
+int mpp_div(const polynomial p, int dp, const polynomial d, int dd, polynomial q, int m, polynomial P);
