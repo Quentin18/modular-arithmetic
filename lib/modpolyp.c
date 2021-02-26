592f25c9acdbfc084d49610ae422c8b42f615147
Quentin18 <quentindeschamps18@orange.fr>
2021-02-26 14:22:23 +0100

    init modpolyp

---
+++ lib/modpolyp.c
@@ -0,0 +1,7 @@
+/**
+ * @file modpolyp.c
+ * @brief Polynomial arithmetic implementation in Fm[x]/P
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "modpolyp.h"
---
+++ lib/modpolyp.h
@@ -0,0 +1,5 @@
+/**
+ * @file modpolyp.h
+ * @brief Header file for polynomial arithmetic in Fm[x]/P
+ */
+#pragma once
